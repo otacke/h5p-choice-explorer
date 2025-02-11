e0ac1d12046ab38aad68d7664a1788e266460ae6
Oliver Tacke <o.tacke@posteo.de>
2025-02-11 11:58:21 +0100

    Refactor entry point

--- src/scripts/h5p-lisum.js
+++ src/scripts/h5p-lisum.js
@@ -14,16 +14,11 @@ export default class Lisum extends H5P.Question {
   constructor(params, contentId, extras = {}) {
     super('lisum');
 
-    // Sanitize parameters
-    const defaults = Util.extend({}, getSemanticsDefaults());
-    this.params = Util.extend(defaults, params);
-
+    this.params = this.sanitizeParameters(params);
     this.contentId = contentId;
     this.extras = extras;
 
-    // Fill dictionary
-    this.dictionary = new Dictionary();
-    this.dictionary.fill({ l10n: this.params.l10n, a11y: this.params.a11y });
+    this.dictionary = new Dictionary().fill({ l10n: this.params.l10n, a11y: this.params.a11y });
 
     this.previousState = extras?.previousState || {};
 
@@ -34,32 +29,9 @@ export default class Lisum extends H5P.Question {
    * Register task introduction text and optional media via H5P.Question.
    */
   registerDomElements() {
-    // Set optional media
-    const media = this.params.media.type;
-    if (media?.library) {
-      const type = media.library.split(' ')[0];
+    this.setIntroductoryMedia(this.params.media.type);
 
-      // Image
-      if (type === 'H5P.Image' && media.params?.file) {
-        this.setImage(media.params.file.path, {
-          disableImageZooming: this.params.media.disableImageZooming,
-          alt: media.params.alt,
-          title: media.params.title,
-          expandImage: media.params.expandImage,
-          minimizeImage: media.params.minimizeImage
-        });
-      }
-      // Video
-      else if (type === 'H5P.Video' && media.params?.sources) {
-        this.setVideo(media);
-      }
-      // Audio
-      else if (type === 'H5P.Audio' && media.params?.files) {
-        this.setAudio(media);
-      }
-    }
-
-    // Register task introduction text
+    // Register task introduction text, setIntroduction and setContent are methods in H5P.Question
     if (this.params.taskDescription) {
       const introduction = document.createElement('div');
       introduction.innerHTML = this.params.taskDescription;
@@ -67,5 +39,33 @@ export default class Lisum extends H5P.Question {
     }
 
     this.setContent(this.main.getDOM());
+  }
+
+  setIntroductoryMedia(media) {
+    if (typeof media?.library !== 'string') {
+      return;
+    }
+
+    const machineName = media.library.split(' ')[0];
+
+    // setImage, setVideo and setAudio are methods in H5P.Question
+    if (machineName === 'H5P.Image' && media.params?.file) {
+      const { path } = media.params.file;
+      const { disableImageZooming } = this.params.media;
+      const { alt, title, expandImage, minimizeImage } = media.params;
+
+      this.setImage(path, { disableImageZooming, alt, title, expandImage, minimizeImage });
+    }
+    else if (machineName === 'H5P.Video' && media.params?.sources) {
+      this.setVideo(media);
+    }
+    else if (machineName === 'H5P.Audio' && media.params?.files) {
+      this.setAudio(media);
+    }
+  }
+
+  sanitizeParameters(params = {}) {
+    const defaults = Util.extend({}, getSemanticsDefaults());
+    return Util.extend(defaults, params);
   }
 }
